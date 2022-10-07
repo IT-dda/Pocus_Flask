6ae8d859131d59c8aaef0fd32908e5c64ef486f5
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-07 16:53:14 +0900

    Feat: 상체 api

--- pocus/views/upper_views.py
+++ pocus/views/upper_views.py
@@ -1,22 +1,70 @@
 from flask import Blueprint, request
-import numpy as np
 import tensorflow as tf
-import cv2
 import mediapipe as mp
-import os
+import numpy as np
+import cv2
+import base64
+from io import BytesIO
+from PIL import Image
+import json
+from ..database import Database
 
 bp = Blueprint('upper', __name__, url_prefix='/upper')
 
-# model = tf.keras.models.load_model('./pocus/static/cnn/binary-holistic-ldmk-3-categorical-softmax-epo20.h5')
-model = tf.keras.models.load_model('./pocus/static/cnn/binary-holistic-ldmk-3-binary-sigmoid-epo20.h5')
+model = tf.keras.models.load_model('./pocus/static/cnn/4-holistic-ldmk-1-binary-sigmoid-epo20.h5')
 
-# mp_pose = mp.solutions.pose
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-# CLASSES = ["correct", "turtle", "shoulder-left", "shoulder-right", "head-left", "head-right", "chin-left", "chin-right"]
-CLASSES = ["correct", "wrong"]
+CLASSES = ["correct", "turtle", "shoulder-left", "shoulder-right", "head-left", "head-right", "chin-left", "chin-right"]
+# CLASSES = ["correct", "wrong"]
+
+
+# image preprocessing : numpy 배열로 바꾸는거까지
+def preprocess(img):
+    img = np.array(img) # <class 'PIL.Image.Image'> -> np array
+
+    # gray scale
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # padding
+    # h, w = img_gray.shape
+    h, w, c = img_rgb.shape
+    dif = w - h
+    # img_padding = cv2.copyMakeBorder(img_gray, dif // 2, dif // 2, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    image = cv2.copyMakeBorder(img_rgb, dif // 2, dif // 2, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+    # image = cv2.cvtColor(img_padding, cv2.COLOR_GRAY2RGB)
+
+    with mp_holistic.Holistic(static_image_mode=True, model_complexity=2, enable_segmentation=True,
+                              refine_face_landmarks=True) as holistic:
+        results = holistic.process(image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        if not results.pose_landmarks and not results.face_landmarks:
+            return None
+
+        # background
+        annotated_image = image.copy()
+        condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
+        bg_image = np.zeros(image.shape, dtype=np.uint8)
+        bg_image[:] = (0, 0, 0)
+        annotated_image = np.where(condition, annotated_image, bg_image)
+        # annotated_image[:] = (0, 0, 0)
+
+        # draw
+        mp_drawing.draw_landmarks(annotated_image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                                  landmark_drawing_spec=None,
+                                  connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style(), )
+        mp_drawing.draw_landmarks(annotated_image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                  landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(), )  # (1920, 1920, 3)
+
+    # annotated_image = cv2.resize(annotated_image, (192, 192))  # (192, 192, 3)
+    annotated_image = cv2.resize(img, (192, 192))  # (192, 192, 3)
+    annotated_image = np.expand_dims(annotated_image, axis=0)
+
+    return annotated_image
 
 
 @bp.route('/', methods=['GET'])
@@ -24,74 +72,32 @@ def upper_get():
     return 'Upper'
 
 
-@bp.route('/predict', methods=['GET'])
+@bp.route('/predict', methods=['GET', 'POST'])
 def predict():
-    res = []
+    if request.method == 'POST':
+        req = request.get_json()
+        url = req['values'][22:]  # "data:image/png;base64," 제거
 
-    BASE_DIR = './pocus/static/images'
-    images = os.listdir(BASE_DIR)
+        # base64 to image
+        img = base64.b64decode(url)
+        img = BytesIO(img)
+        img = Image.open(img)
+        img = img.convert("RGB")
 
-    for i in images:
-        img = cv2.imread(f'{BASE_DIR}/{i}')
-        if not img.any():
-            return 'nothing to predict'
+        # if not img.any():
+        #     return json.dumps({'message': 'cannot read image'})
 
+        annotated_image = preprocess(img)
 
-        # image preprocessing : numpy 배열로 바꾸는거까지
-        # with mp_pose.Pose(static_image_mode=True, model_complexity=2, enable_segmentation=True, min_detection_confidence=0.5) as pose:
-        with mp_holistic.Holistic(static_image_mode=True, model_complexity=2, enable_segmentation=True, refine_face_landmarks=True) as holistic:
-            # gray scale
-            # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            # padding
-            # h, w = img_gray.shape
-            h, w, c = img_rgb.shape
-            dif = w - h
-            # img_padding = cv2.copyMakeBorder(img_gray, dif // 2, dif // 2, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
-            image = cv2.copyMakeBorder(img_rgb, dif // 2, dif // 2, 0, 0, cv2.BORDER_CONSTANT, value=[0, 0, 0])
-            # image = cv2.cvtColor(img_padding, cv2.COLOR_GRAY2RGB)
-            results = holistic.process(image)
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-            if not results.pose_landmarks and not results.face_landmarks:
-                res.append(f'{i} -> 판단 불가')
-                continue
-
-            # background
-            annotated_image = image.copy()
-            condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
-            bg_image = np.zeros(image.shape, dtype=np.uint8)
-            bg_image[:] = (0, 0, 0)
-            annotated_image = np.where(condition, annotated_image, bg_image)
-            # annotated_image[:] = (0, 0, 0)
-
-            # draw
-            mp_drawing.draw_landmarks(annotated_image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION, landmark_drawing_spec=None, connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style(), )
-            mp_drawing.draw_landmarks(annotated_image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(), )  # (1920, 1920, 3)
-
-            # cv2.imwrite(f'./{i}', annotated_image)
-            # annotated_image = cv2.resize(annotated_image, (192, 192))  # (192, 192, 3)
-            annotated_image = cv2.resize(img, (192, 192))  # (192, 192, 3)
-            target = annotated_image.astype(np.float32) / 255.  # (192, 192, 3)
-            annotated_image = np.expand_dims(annotated_image, axis=0)
-
-        annotated_image = cv2.resize(img, (192, 192))  # (192, 192, 3)
-        target = annotated_image.astype(np.float32) / 255.  # (192, 192, 3)
-        annotated_image = np.expand_dims(annotated_image, axis=0)
+        if annotated_image is None:
+            return json.dumps({'message': 'cannot get landmarks'})
 
         prediction = model.predict(annotated_image)  # <class 'numpy.ndarray'>
-        label = CLASSES[prediction.argmax()]
+        label = prediction.argmax()
 
         print(prediction)
-        # if 손 인식: # 근데 이러면 손을 들고 있는 자세는?
-        #     label = CLASSES[prediction.argmax(prediction[6:])]
-        # else: # 손 없음
-        #     label = CLASSES[prediction[:6].argmax()]
 
-        res.append(f'{i} -> {label}')
-
-    # 방법1) 라벨을 숫자로 리턴 -> node에서 변환
-    # 방법2) 라벨을 클래스명으로 바꿔서 리턴
-    return f'{res}'
-
+        # return json.dumps({'message': label})
+        return json.dumps({'message': CLASSES[label]})
+    else:  # GET
+        return f'upper predict test'
