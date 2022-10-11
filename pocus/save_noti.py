d5190b3b3ccfc9a16e605d94013a437a2fbb83e1
jeong-yeji <jeongyeji.mail@gmail.com>
2022-10-12 01:26:58 +0900

    Feat: 알림 저장 함수

---
+++ pocus/save_noti.py
@@ -0,0 +1,23 @@
+from database import Database
+
+db = Database()
+
+
+def table_log(p1, p2, p3):
+    sql = f"INSERT INTO pocus.Log(user_id, warning, isUpper) VALUES ({p1}, '{p2}', {p3})"
+    db.execute(sql)
+    db.commit()
+
+    pk = db.executeAll("SELECT LAST_INSERT_ID()")
+
+    return pk[0]['LAST_INSERT_ID()']
+
+
+def table_ss(p1, s1, s2, s3, s4):
+    sql = f"INSERT INTO pocus.SS(log_id, ss1, ss2, ss3, ss4) VALUES ({p1}, {s1}, {s2}, {s3}, {s4})"
+    db.execute(sql)
+    db.commit()
+
+    pk = db.executeAll("SELECT LAST_INSERT_ID()")
+
+    return pk[0]['LAST_INSERT_ID()']
